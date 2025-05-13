8a67c260ea541c36560d273d70c0d2de977ac953
M Sulaiman Khan <sulaimanjan288@gmail.com>
2025-05-13 19:44:28 +0500

    "second commit"

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,34 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>Cloud Computing Lab 12</h1>
+      <h2>Muhammad Sulaiman</h2>
+      {/* <h2>Adil Khan</h2>
+      <h2>Irfan Zeerak</h2>
+      <h2>Sikander Afridi</h2>
+      <h2>jalal</h2> */}
+
+      <nav className="nav">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+
+      <h2>Navigation Header</h2>
+      <nav className="nav">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+
+      <h2>Home</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
